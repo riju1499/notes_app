5f380f7b658e0987a66f4d46535c767e0fc384ea
Riju Phaiju <rijuphaiju@gmail.com>
2024-04-07 21:29:53 +0545

    second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,5 +127,4 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL='/smart/notes'
+
